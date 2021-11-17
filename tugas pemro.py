f38bf241aadcf22ec65374b98f7c55b794128869
Eka Prasetya Nugraha <92531556+Ekaatroni@users.noreply.github.com>
2021-11-17 21:35:44 +0700

    Add files via upload

---
+++ tugas pemro.py
@@ -0,0 +1,12 @@
+nama = "Eka Prasetya nugraha" #tipe datanya string, karena memuat kata atau sekumpulan huruf
+print ("Nama : ", nama)
+nim = 212410102074 #tipe datanya integer, karena memuat bilangan
+print ("NIM : ", nim)
+umur = 19 #tipe datanya integer, karena memuat bilangan
+print("Umur : ", umur, "tahun")
+tinggi = 1.71 #tipe datanya float, karena memuat bilangan desimal
+print ("Tinggi badan :", tinggi, "m")
+berat = 70 #tipe datanya integer, karena memuat bilangan
+print ("Berat badan :", berat, "kg")
+status = True #tipe datanya boolean, karena memuat dua nilai yakni true or false
+print ("status Kejombloan :", status, "jomblo sejak di lauhul mahfudz")
